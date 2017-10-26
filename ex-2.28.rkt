d38f1f107034e8f4ace54eb6cfcd6b536d9457d7
kalashnikov <kalaexj@gmail.com>
2017-10-27 00:20:25 +0800

    Simpler version

--- ex-2.28.rkt
+++ ex-2.28.rkt
@@ -1,12 +1,21 @@
 #lang racket
 
-(define (fringe x)
-  (define (helper x l)
-    (cond ((null? x) l)
-          ((integer? x) (cons x l))
-          ((list? x) (helper (cadr x) (helper (car x) l))) 
-          (else (helper (cdr x) (helper (car x) l)))))
-  (reverse (helper x '())))
+; (define (fringe x)
+;   (define (helper x l)
+;     (cond ((null? x) l)
+;           ((integer? x) (cons x l))
+;           ((list? x) (helper (cadr x) (helper (car x) l))) 
+;           (else (helper (cdr x) (helper (car x) l)))))
+;   (reverse (helper x '())))
+
+
+; Ref: http://community.schemewiki.org/?sicp-ex-2.28
+; By zhenhuaa
+(define (fringe x) 
+  (cond ((null? x) x) 
+        ((number? x) (list x)) 
+        (else (append (fringe (car x)) 
+                      (fringe (cdr x)))))) 
 
 (define x (list (list 1 2) (list 3 4)))
 
